5372fe0b4d03b37574b67103d1166d9a4a6728dc
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-14 12:50:35 -0600

    Made minor code change for character selection

--- src/textAdvGame/GameApplication.java
+++ src/textAdvGame/GameApplication.java
@@ -8,13 +8,7 @@ public class GameApplication {
         System.out.println("\"You wake up in the Enchanted Forest, unsure of how you got there. The forest you are in has been\n" +
                 "known for it's strange properties; all adventurers who enter the forest never return as the same\n" +
                 "person...or even return at all. As you rub the sleep out of your eyes, you begin to remember who you" +
-                " are.\n" +
-                "You are...:\n" +
-                "[1] The Brave Warrior\n" +
-                "[2] The Persistent Mage\n" +
-                "[3] The Agile Thief\n" +
-                "[4] The Empathetic Cleric\n" +
-                "[5] The Lone Witch Hunter");
+                " are.\n");
     }
 
     public static void main(String[] args) {
@@ -23,7 +17,12 @@ public class GameApplication {
 
         boolean running = true;
         while(running) {
-            System.out.println("Select a number 1 to 5 to choose your character.");
+            System.out.println("You are...:\n" +
+                    "[1] The Brave Warrior\n" +
+                    "[2] The Persistent Mage\n" +
+                    "[3] The Agile Thief\n" +
+                    "[4] The Empathetic Cleric\n" +
+                    "[5] The Lone Witch Hunter");
             String userChoice = scanner.nextLine();
             if (userChoice.equals("1")) {
                 System.out.println("\"You are the warrior, a person full of bravery and driven by your passion to protect others. You came\n" +
