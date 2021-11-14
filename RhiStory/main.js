cdb9b342aa15432a59dc202d693c89f9bef13888
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-14 13:32:16 -0600

    Added Attack Menu Instructions

--- RhiStory/main.js
+++ RhiStory/main.js
@@ -129,5 +129,152 @@ Are you sure this is your name? [Y/N] //Make user confirm their name
 //Return a message like this and start the game officially
 "You are (name here), The (character the user picked. Brave Warrior, Persistent Mage, etc.)"
 
+----------------------
+
+AFTER USER SELECTION:
+
+"After you remember your name and your past, you get up and begin to search The Enchanted Forest,
+but suddenly you hear a noise from behind you!"
+
+A GOBLIN appears! What would you like to do? //Goblin Health: 100, Attack: 5-10 range
+
+
+//MENU FOR USERS, NO MATTER THE CLASS:
+
+[1] Attack //Lets user attack based on damage amount
+[2] Trait //Activates Skill based on class
+[3] Inventory //Will be added for more usefulness later, will be a txt file.
+[4] Surrender //Ends the game.
+
+----------------------
+
+//How an [1] Attack message might look:
+
+"You swing your sword and do x damage!" //Warrior
+"You cast a spell and do x damage!" //Mage
+"You slash the enemy with your blades and do x damage!" //Thief
+"You throw a harmful elixir and do x damage!" //Cleric
+"You shoot your crossbow and do x damage!" //Witch Hunter
+
+----------------------
+
+//How an enemy attack message might look:
+
+"The goblin swings its hammer at you! It did x damage!" //Goblins have 1 attack
+//More complex enemies might have multiple moves.
+
+----------------------
+
+//How an [2] Trait message might look:
+
+WARRIOR
+
+"Your trait is BRAVERY. You have a 50% chance to do double the set damage.
+This is a trait that is always active." //Warrior
+//Take them back to the main attack menu after they view their trait
+
+
+//IF warrior does double damage, show a message like this.
+
+"You showed BRAVERY! You swing your sword valiantly, doing x damage!"
+
+-------
+
+MAGE
+
+"Your trait is PERSISTENCE. You have a 25% chance to do a second attack.
+This is a trait that is always active." //Mage
+//Take them back to the main attack menu after they view their trait
+
+
+//IF mage does double second attack, show a message like this
+
+"You showed PERSISTENCE! Determined to not let the enemy get the best of you,
+you cast a second spell, doing x damage!"
+
+-------
+
+THIEF
+
+"Your trait is AGILITY. You can run away from a battle. Can only be used
+three times. Would you like to use your trait? [Y/N]" //Thief
+
+
+//IF thief decides to run away, show a message like this
+
+"You showed AGILITY! You decided the battle was not one worth winning, so you
+make a tactful retreat! You can run away x more times."
+//After this, continue game as normal.
+
+
+//IF thief decides to run away but has no more tries, show a message like this
+
+"You tried to show AGILITY, but the enemy corners you! You can't run away anymore."
+//Take thief back to attack menu
+
+-------
+
+CLERIC
+
+"Your trait is EMPATHY. Gives you the ability to heal yourself in battle.  Would you like to use
+your trait? [Y/N]" //Cleric
+
+
+//IF cleric decides to heal themself, show a message like this
+
+"You showed EMPATHY! You drink a magical elixir and gain x health! Your total health is now x."
+//After this, let enemy attack. Acts as a turn. Unlimited uses.
+
+-------
+
+WITCH HUNTER
+
+"Your trait is ADAPTABILITY. Gives you a 25% chance to dodge any attack.
+This is a trait that is always active." //Witch Hunter
+//Take them back to the main attack menu after they view their trait
+
+//IF witch hunter dodges, show a message like this.
+
+"You showed ADAPTABILITY! You predicted the enemies moves and dodged their attack!"
+//After this, let user attack.
+
+-------
+
+If any class picks [3] Inventory right now:
+
+"You have nothing in your inventory!" //Take them back to attack menu.
+
+-------
+
+If any class picks [4] Surrender:
+
+"Surrendering the fight will cause the game to end. Are you SURE you want to surrender?" [Y/N]
+//If user picks no, take them back to attack menu. Otherwise, end game and display a message
+like this:
+
+"You surrendered and The Witch of The Enchanted Forest continued their evil work. GAME OVER!"
+
+-------
+
+Things to keep in mind:
+
+-Make sure when the user wants to view or use their trait, it pertains to their class.
+We don't want the user to type in what their class is or choose their trait, the game should
+already know based on the class they chose before.
+
+- Don't let users view or use other traits that don't belong to them
+(unless specified later down the road).
+
+-------
+
+WHEN GOBLIN IS DEFEATED
+
+//Show a message like this when goblin is defeated
+
+"You slayed the GOBLIN!"
+
+STORY CONTINUED:
+
+"You continue your journey into The Enchanted Forest."
 
  */
