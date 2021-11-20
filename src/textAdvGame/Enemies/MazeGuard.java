3f8fc7c5e9bbe8c1e236cc101968380af165479f
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-19 18:48:42 -0600

    Got Special Attacks Working

--- src/textAdvGame/Enemies/MazeGuard.java
+++ src/textAdvGame/Enemies/MazeGuard.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class MazeGuard extends Enemy{
 
     private int enemyHealth;
-    private int specialAttackChance = 25;  //percent chance
+    private int specialAttackChance = 75;  //percent chance
 
     Random random = new Random();
     private int enemyAttackDamage;
@@ -33,6 +33,20 @@ public class MazeGuard extends Enemy{
         return "*** Put a description here. ***";
     }
 
+    public int getSpecialAttackDamage() {
+        return this.enemyAttackDamage * 2;
+    }
+
+
 }
 //if the enemy has a special trait, put that number into the variable;
 //if specialAttack != 0, then do the attack
+
+
+//Maze Guard
+//Chance: 25
+//Double the damage already done
+
+//Boulder Monster
+//Chance: 50
+//will always do 100 damage
