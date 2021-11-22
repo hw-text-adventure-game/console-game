801af7b7269ad77cdf8d05d4c84019f49ee7efb8
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-21 19:49:59 -0600

    Added TODO JS File

---
+++ RhiStory/todo.js
@@ -0,0 +1,28 @@
+/* (This is the easiest file type to comment in.)
+
+--------------- OVERALL/URGENT TODOS ---------------
+
+[] Add Cleric Functionality
+[] Do testing with each class, make them all look similar to WARRIOR outputs.
+[] Add a functioning inventory system for each character. (Using arraylists?)
+[] Don't allow player to use/pick an item they don't have in battle.
+
+
+---- MAZE TODOS ----
+
+[✔] Finish thirdMazePaths -Rhi
+[] Finish fourthMazePaths
+[] Finish fifthMazePaths
+[] When going down a path to get an item, add item to inventory and program functionality of item.
+
+
+-- Minor TODOS ---
+
+[] Refactor attackMenu to make it less cluttered
+
+
+
+
+
+
+ */
